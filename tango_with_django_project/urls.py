7639d216c44bc8a32140aa6ba685e8c4ea96d975
Matthew Cole <2753620C@student.gla.ac.uk>
2024-01-13 22:22:03 +0000

    Commit 7 13/01/2024 -> Up to 4.4

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 # different web apps
 from rango import views
@@ -28,4 +32,4 @@ urlpatterns = [
     path('rango/', include('rango.urls')),
     path('tango_app2/', include('tango_app2.urls')),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
