ba2033ee01801293acf381eaacc368db7ed0f0ce
Matthew Cole <2753620C@student.gla.ac.uk>
2024-01-13 17:50:14 +0000

    Commit 4 13/01/2024 -> Chapter 3 Complete

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+                        # these return html code
+    return HttpResponse("Rango says hey there partner! <br> To go to the about page, click <a href='http://127.0.0.1:8000/rango/about'>here</a>")
     
 def about(request):
-    return HttpResponse("Rango says here is the about page.")
+    return HttpResponse("Rango says here is the about page. <br> To go to the index page, click <a href='http://127.0.0.1:8000/rango/'>here</a>")
+    
